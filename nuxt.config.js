8f6a7d8c121070f682dc0a0d9d5e24172c01ce31
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-10-15 22:10:53 +0000

    added the modules

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,6 +28,7 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    buildModules: ['@nuxtjs/svg', '@nuxtjs/style-resources']
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
