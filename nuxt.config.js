d080c9a32543529015dc8057f042090575b5ebdd
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-10-16 17:26:03 +0000

    added the plugins for visibility and content plcaeholder

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,6 +24,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~/plugins/vue-placeholders.js',
+    '~/plugins/vue-observe-visibility.client.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
