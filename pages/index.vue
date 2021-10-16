c9214aabed8c45f89e7de3ddbd02786d38cfe0f4
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-10-16 20:36:12 +0000

    added the blocs for artiels and index

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,35 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        blogg
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <div class="page-wrapper">
+    <div class="article-cards-wrapper">
+      <article-card-block
+        v-for="(article, i) in articles"
+        :key="article.id"
+        :article="article"
+        class="article-card-block"
+      />
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+  import ArticleCardBlock from '@/components/blocks/ArticleCardBlock'
+
+  export default {
+    components: {
+      ArticleCardBlock
+    },
+    data() {
+      return {
+        currentPage: 1,
+        articles: []
+      }
+    },
+    async fetch() {
+      const articles = await fetch(
+        `https://dev.to/api/articles?tag=nuxt&state=rising&page=${this.currentPage}`
+      ).then(res => res.json())
+
+      this.articles = this.articles.concat(articles)
+    }
+  }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
